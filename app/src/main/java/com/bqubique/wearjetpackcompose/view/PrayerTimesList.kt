11f43e5f95c2ab1748a520b386e69c6d01518c81
bqubique <blinnqipa@gmail.com>
2022-04-17 15:06:39 +0200

    Select next prayer improvements. Modularize rendering prayer cards. Update libraries.

--- app/src/main/java/com/bqubique/wearjetpackcompose/view/PrayerTimesList.kt
+++ app/src/main/java/com/bqubique/wearjetpackcompose/view/PrayerTimesList.kt
@@ -3,9 +3,11 @@ package com.bqubique.wearjetpackcompose.view
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,9 +18,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.*
 import com.bqubique.wearjetpackcompose.R
+import com.bqubique.wearjetpackcompose.model.PrayerTimes
 import com.bqubique.wearjetpackcompose.viewmodel.PrayerTimesViewModel
 
-@OptIn(ExperimentalWearMaterialApi::class)
 @Composable
 fun PrayerTimesList(
     prayerTimesViewModel: PrayerTimesViewModel,
@@ -34,8 +36,6 @@ fun PrayerTimesList(
         timeText = { TimeText() },
         positionIndicator = { PositionIndicator(scalingLazyListState = scalingLazyListState) }
     ) {
-        Log.d("PrayerTimesList", "PrayerTimesList: ${nextPrayer.value}")
-
         if (loading.value!!)
             CircularProgressIndicator(
                 modifier = Modifier.fillMaxSize(),
@@ -52,17 +52,13 @@ fun PrayerTimesList(
                     .fillMaxSize()
                     .padding(top = 0.dp, bottom = 0.dp),
             ) {
-
                 item {
                     TitleCard(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(start = 10.dp, end = 10.dp, top = 40.dp, bottom = 10.dp),
                         onClick = {},
-//                        backgroundPainter = CardDefaults.imageWithScrimBackgroundPainter(
-//                            backgroundImagePainter = painterResource(id = R.drawable.sunset),
-//                        ),
-                        backgroundPainter = if (nextPrayer.value == 0) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
+                        backgroundPainter = CardDefaults.cardBackgroundPainter(),
                         time = { Text("Imsaku") },
                         title = {
                             Text(
@@ -72,104 +68,55 @@ fun PrayerTimesList(
                         },
                     ) {}
                 }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 1) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-                        time = { Text("Sabahu") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.sabahu.toString(),
-                                color = Color(0xffffc400)
-                            )
+                items(6) { count ->
+                    PrayerTimeCard(
+                        nextPrayer = nextPrayer,
+                        prayerTime = when (count) {
+                            0 -> prayerTimes.value?.get(0)?.sabahu.toString()
+                            1 -> prayerTimes.value?.get(0)?.lindjaDiellit.toString()
+                            2 -> prayerTimes.value?.get(0)?.dreka.toString()
+                            3 -> prayerTimes.value?.get(0)?.ikindia.toString()
+                            4 -> prayerTimes.value?.get(0)?.akshami.toString()
+                            else -> prayerTimes.value?.get(0)?.jacia.toString()
                         },
-                    ) {}
-                }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 2) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-                        time = { Text("Lindja e Diellit") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.lindjaDiellit.toString(),
-                                color = Color(0xffffc400)
-                            )
+                        count = count,
+                        title = when (count) {
+                            0 -> "Sabahu"
+                            1 -> "Lindja e Diellit"
+                            2 -> "Dreka"
+                            3 -> "Ikindia"
+                            4 -> "Akshami"
+                            else -> "Jacia"
                         },
-                    ) {}
-                }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 3) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-                        time = { Text("Dreka") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.dreka.toString(),
-                                color = Color(0xffffc400)
-                            )
-                        },
-                    ) {}
-                }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 4) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-                        time = { Text("Ikindia") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.ikindia.toString(),
-                                color = Color(0xffffc400)
-                            )
-                        },
-                    ) {}
-                }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 5) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-                        time = { Text("Akshami") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.akshami.toString(),
-                                color = Color(0xffffc400)
-                            )
-                        },
-                    ) {}
-                }
-                item {
-                    TitleCard(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 10.dp, end = 10.dp, bottom = 0.dp),
-                        onClick = {},
-                        backgroundPainter = if (nextPrayer.value == 6) CardDefaults.imageWithScrimBackgroundPainter(painterResource(id = R.drawable.sunset)) else CardDefaults.cardBackgroundPainter(),
-
-                        time = { Text("Jacia") },
-                        title = {
-                            Text(
-                                text = prayerTimes.value?.get(0)?.jacia.toString(),
-                                color = Color(0xffffc400)
-                            )
-                        },
-                    ) {}
+                    )
                 }
             }
     }
+}
+
+@Composable
+fun PrayerTimeCard(
+    nextPrayer: State<Int?>,
+    prayerTime: String,
+    count: Int,
+    title: String
+) {
+    return TitleCard(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp)
+            .height(if (nextPrayer.value == count) 90.dp else 45.dp),
+        onClick = {},
+        backgroundPainter = if (nextPrayer.value == count) CardDefaults.imageWithScrimBackgroundPainter(
+            painterResource(id = R.drawable.sunset)
+        ) else CardDefaults.cardBackgroundPainter(),
+        time = { Text(title) },
+        title = {
+            Text(
+                text = prayerTime,
+                color = Color(0xffffc400)
+            )
+        },
+    ) {}
 }
 
--- app/src/main/java/com/bqubique/wearjetpackcompose/viewmodel/PrayerTimesViewModel.kt
+++ app/src/main/java/com/bqubique/wearjetpackcompose/viewmodel/PrayerTimesViewModel.kt
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 import javax.inject.Inject
 
-const val TAG = "MainViewModel"
+const val TAG = "PrayerTimesViewModel"
 
 @HiltViewModel
 class PrayerTimesViewModel @Inject constructor(
@@ -49,7 +49,6 @@ class PrayerTimesViewModel @Inject constructor(
     private fun selectCurrentPrayer() {
         if (_prayerTimes.value.isNullOrEmpty().not()) {
             val prayerTimesHours = listOf(
-                _prayerTimes.value?.get(0)?.imsaku?.substringBefore(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.sabahu?.substringBefore(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.lindjaDiellit?.substringBefore(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.dreka?.substringBefore(":")?.toInt()!!,
@@ -59,7 +58,6 @@ class PrayerTimesViewModel @Inject constructor(
             )
 
             val prayerTimesMinutes = listOf(
-                _prayerTimes.value?.get(0)?.imsaku?.substringAfter(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.sabahu?.substringAfter(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.lindjaDiellit?.substringAfter(":")?.toInt()!!,
                 _prayerTimes.value?.get(0)?.dreka?.substringAfter(":")?.toInt()!!,
@@ -73,90 +71,93 @@ class PrayerTimesViewModel @Inject constructor(
             val currentHour = rightNow.get(Calendar.HOUR_OF_DAY)
             val currentMinute = rightNow.get(Calendar.HOUR_OF_DAY)
 
-            val time: LocalTime = LocalTime.of(currentHour, currentMinute)
-
-            for (i in prayerTimesHours.indices) {
-                if (prayerTimesHours[i]!! > currentHour) {
-                    when (i) {
+            for(i in prayerTimesHours.indices){
+                if(prayerTimesHours[i] == currentHour){
+                    when(i){
                         0 -> {
-                            //Imsak
-                            _nextPrayer.value = 0
-                            return
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","Sabahu")
+                                _nextPrayer.value = 0
+                            }else{
+                                Log.d("ViewModel","real Lindja e Diellit")
+                                _nextPrayer.value = 1
+                            }
                         }
                         1 -> {
-                            //Fajr
-                            _nextPrayer.value = 1
-                            return
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","Lindja e Diellit")
+                                _nextPrayer.value = 1
+                            }else{
+                                Log.d("ViewModel","real Dreka")
+                                _nextPrayer.value = 2
+                            }
                         }
                         2 -> {
-                            //Sunrise
-                            _nextPrayer.value = 2
-                            return
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","Dreka")
+                                _nextPrayer.value = 2
+                            }else{
+                                Log.d("ViewModel","real Ikindia")
+                                _nextPrayer.value = 3
+                            }
                         }
                         3 -> {
-                            //Dhuhr
-                            _nextPrayer.value = 3
-                            return
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","Ikindia")
+                                _nextPrayer.value = 3
+                            }else{
+                                Log.d("ViewModel","real Akshami")
+                                _nextPrayer.value = 4
+                            }
                         }
                         4 -> {
-                            //Asr
-                            _nextPrayer.value = 4
-                            return
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","real Akshami")
+                                _nextPrayer.value = 4
+                            }else{
+                                Log.d("ViewModel","real Jacia")
+                                _nextPrayer.value = 5
+                            }
                         }
                         5 -> {
-                            //Maghrib
-                            _nextPrayer.value = 5
-                            return
-                        }
-                        6 -> {
-                            //Isha
-                            _nextPrayer.value = 6
-                            return
-                        }
-                    }
-                } else if (prayerTimesHours[i]!! == currentHour) {
-                    if (prayerTimesMinutes[i]!! <= currentMinute) {
-                        when (i) {
-                            0 -> {
-                                //Imsak
-                                _nextPrayer.value = 0
-                                return
-                            }
-                            1 -> {
-                                //Fajr
-                                _nextPrayer.value = 1
-                                return
-                            }
-                            2 -> {
-                                //Sunrise
-                                _nextPrayer.value = 2
-                                return
-                            }
-                            3 -> {
-                                //Dhuhr
-                                _nextPrayer.value = 3
-                                return
-                            }
-                            4 -> {
-                                //Asr
-                                _nextPrayer.value = 4
-                                return
-                            }
-                            5 -> {
-                                //Maghrib
+                            if(currentMinute<=prayerTimesMinutes[i]){
+                                Log.d("ViewModel","real Jacia")
                                 _nextPrayer.value = 5
-                                return
-                            }
-                            6 -> {
-                                //Isha
+                            }else{
+                                Log.d("ViewModel","real Sabahu")
                                 _nextPrayer.value = 6
-                                return
                             }
                         }
                     }
-                } else {
-                    //Imsak
-                    _nextPrayer.value = 0
+                    return
+                }else if(prayerTimesHours[i] > currentHour){
+                    when(i){
+                        0 ->{
+                            Log.d("ViewModel","Sabahu")
+                            _nextPrayer.value = 0
+                        }
+                        1 ->{
+                            Log.d("ViewModel","Lindja e diellit")
+                            _nextPrayer.value = 1
+                        }
+                        2 ->{
+                            Log.d("ViewModel","Dreka")
+                            _nextPrayer.value = 2
+                        }
+                        3 ->{
+                            Log.d("ViewModel","Ikindia")
+                            _nextPrayer.value = 3
+                        }
+                        4 ->{
+                            Log.d("ViewModel","Akshami")
+                            _nextPrayer.value = 4
+                        }
+                        5 ->{
+                            Log.d("ViewModel","Jacia")
+                            _nextPrayer.value = 5
+                        }
+                    }
+                    return
                 }
             }
         }
